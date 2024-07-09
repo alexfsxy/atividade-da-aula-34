03f016848a4e030af7ba7ca0b634c50c7293071c
Ferreira <98367479+alexfsxy@users.noreply.github.com>
2024-07-09 02:30:56 -0300

    Create HelloWorld.jsx
    Atividade da aula 34 no módulo 4, do curso mais pra Ti

---
+++ HelloWorld.jsx
@@ -0,0 +1,23 @@
+Encaminhe o link do Github do seu header com React.
+
+function HelloWorld() {
+    return(
+        <body> 
+         <header>
+        <a href="#" id="logo" class="nav-item">
+        <h2><i class="material-icons">Hello Word</i>Mundo melhor</h2></a>
+        <nav class="nav-item">
+        <ul>
+            <a href="#about"><li>Profissão</li></a>
+            <a href="#employees-container"><li>Esporte</li></a>
+            <a href="#account"><li>Lazer</li></a>
+        </ul>
+        </nav>
+    </header>
+   <div>
+    <h1>Oi meu nome Alex</h1>
+    <p>Pode me chamar Alexsandro Ferreira</p>
+   </div>
+</body>
+    )
+}
